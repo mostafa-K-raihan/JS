1f466424fc595c5c3764999da532ced136cec928
Mostafa Khalid Raihan <mraihan29@gmail.com>
2018-06-10 05:58:30 +0600

    audio files updated, sound all ok. no sound if other than specified keys are pressed.

--- drumkit/main.js
+++ drumkit/main.js
@@ -1,3 +1,8 @@
 window.addEventListener('keydown', (e) => {
-    console.log(e);
+    
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    
 })
